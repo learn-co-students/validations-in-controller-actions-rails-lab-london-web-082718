46479b10a1832f453814c447e7f0f753a912b4db
thexyman <makdaa1@gmail.com>
2018-09-24 10:36:27 +0100

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  validates_presence_of :title
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validates_length_of :content, :minimum => 100
 end
