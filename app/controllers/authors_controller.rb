71d7daea18064d9e4f1e9e77f304ea10591e9718
klbennett <https://github.com/settings/applications>
2018-09-26 13:43:34 +0100

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,10 +7,24 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create!(author_params)
-
+    # Create a brand new, unsaved, not-yet-validated Author object from the form.
+    @author = Author.new(author_params)
+    # Run the validations WITHOUT attempting to save to the database, returning
+    # true if the Author is valid, and false if it's not.
+    if @author.valid?
+      @author.save
     redirect_to author_path(@author)
+  else
+    # If the post is invalid, hold on to @author, because it is now full of
+      # useful error messages, and re-render the :new page, which knows how
+      # to display them alongside the user's entries.
+    render :new
+    # render can be instructed to render the templates from other actions. In the above code, since we want the :new template from the same controller,
+    # we don't have to specify anything except the template name.
+    # Remember: redirects incur a new page load. When we redirect after validation failure,
+    # we lose the instance of author that has feedback (messages for the user) in its errors attribute
   end
+end
 
   private
 
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,11 @@
 class Author < ActiveRecord::Base
+#   The name cannot be blank
+# The e-mail is unique
+
+validates :name, presence: true
+validates :name, uniqueness: true
+validates :email, presence: true
+validates :email, uniqueness: true
+
+
 end
